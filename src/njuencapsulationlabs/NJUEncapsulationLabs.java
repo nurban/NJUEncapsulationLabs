d6467baa7d05554c8c33dd9fdf47b13197ef183b
nurban <nurban@L11711038612.wctc.msft>
2012-08-29 18:15:15 -0500

    changed my mind...

--- src/njuencapsulationlabs/NJUEncapsulationLabs.java
+++ src/njuencapsulationlabs/NJUEncapsulationLabs.java
@@ -15,5 +15,6 @@ public class NJUEncapsulationLabs {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Cya!");
     }
 }
---
+++ src/njuencapsulationlabs/SecondClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njuencapsulationlabs;
+
+/**
+ *
+ * @author nurban
+ */
+public class SecondClass {
+    private int x;
+    
+}
